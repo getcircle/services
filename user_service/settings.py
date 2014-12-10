cf892eba447f5b28956b69c4e4c12429eb4ac451
Michael Hahn <mwhahn@gmail.com>
2014-12-09 19:17:58 -0800

    Specify postgres db backend

--- user_service/settings.py
+++ user_service/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'user_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'users',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
