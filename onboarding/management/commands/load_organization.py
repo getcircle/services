10c9eacd8d0b77f27493756735cc4af87d648505
Michael Hahn <mwhahn@gmail.com>
2014-12-31 09:57:28 -1000

    Add `verbose` flag to the `load_organization` command

--- onboarding/management/commands/load_organization.py
+++ onboarding/management/commands/load_organization.py
@@ -14,18 +14,32 @@ class Command(BaseCommand):
     args = '<organization domain> <filename>'
     help = 'Loads the onboarding file into the organization'
     option_list = BaseCommand.option_list + (
-        make_option('--commit',
+        make_option(
+            '--commit',
             action='store_true',
             dest='commit',
             default=False,
-            help='Commit the parsed data'),
-        )
+            help='Commit the parsed data',
+        ),
+        make_option(
+            '--verbose',
+            action='store_true',
+            dest='verbose',
+            default=False,
+            help='Run the command with debug logging',
+        ),
+    )
 
     def handle(self, *args, **options):
         organization_domain = args[0]
         filename = args[1]
 
-        parser = Parser(organization_domain, filename, 'admin-token')
+        parser = Parser(
+            organization_domain=organization_domain,
+            filename=filename,
+            token='admin-token',
+            verbose=options['verbose'],
+        )
         try:
             parser.parse(commit=options['commit'])
         except ParseError as e:
