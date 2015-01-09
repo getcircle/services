dafb4db2534d3dbad6592e8e66d82898866214d3
Michael Hahn <mwhahn@gmail.com>
2015-01-08 20:36:04 -0800

    Add DirectReports landing category

--- landing/actions.py
+++ landing/actions.py
@@ -21,6 +21,9 @@ class GetCategories(actions.Action):
             # XXX handle errors properly
             raise Exception('failed to fetch peers')
 
+        if not len(response.result.profiles):
+            return
+
         peers = self.response.profile_categories.add()
         peers.title = 'Peers'
         peers.content_key = 'title'
@@ -28,5 +31,25 @@ class GetCategories(actions.Action):
             container = peers.content.add()
             container.CopyFrom(profile)
 
+    def _get_direct_reports_category(self):
+        response = self.profile_client.call_action(
+            'get_direct_reports',
+            profile_id=self.request.profile_id,
+        )
+        if not response.success:
+            # XXX handle errors better
+            raise Exception('failed to fetch direct reports')
+
+        if not len(response.result.profiles):
+            return
+
+        reports = self.response.profile_categories.add()
+        reports.title = 'Direct Reports'
+        reports.content_key = 'title'
+        for profile in response.result.profiles:
+            container = reports.content.add()
+            container.CopyFrom(profile)
+
     def run(self, *args, **kwargs):
         self._get_peers_category()
+        self._get_direct_reports_category()
--- landing/tests.py
+++ landing/tests.py
@@ -24,17 +24,12 @@ class TestLandingService(TestCase):
         profile.title = fuzzy.FuzzyText().fuzz()
         profile.full_name = fuzzy.FuzzyText().fuzz()
 
-    def test_profile_category_invalid_profile_id(self):
-        response = self.client.call_action('get_categories', profile_id='invalid')
-        self._verify_field_error(response, 'profile_id')
-
-    def test_peers_profile_category(self):
+    def _mock_get_peers(self, profile_id, peers=3):
         mock_response = mock.get_mockable_response('profile', 'get_peers')
-        for _ in range(3):
+        for _ in range(peers):
             profile = mock_response.profiles.add()
             self._mock_profile(profile)
 
-        profile_id = fuzzy.FuzzyUUID().fuzz()
         mock.instance.register_mock_response(
             'profile',
             'get_peers',
@@ -42,11 +37,47 @@ class TestLandingService(TestCase):
             profile_id=profile_id,
         )
 
+    def _mock_get_direct_reports(self, profile_id, direct_reports=3):
+        mock_response = mock.get_mockable_response('profile', 'get_direct_reports')
+        for _ in range(direct_reports):
+            profile = mock_response.profiles.add()
+            self._mock_profile(profile)
+
+        mock.instance.register_mock_response(
+            'profile',
+            'get_direct_reports',
+            mock_response,
+            profile_id=profile_id,
+        )
+
+    def test_profile_category_invalid_profile_id(self):
+        response = self.client.call_action('get_categories', profile_id='invalid')
+        self._verify_field_error(response, 'profile_id')
+
+    def test_peers_profile_category(self):
+        profile_id = fuzzy.FuzzyUUID().fuzz()
+        self._mock_get_peers(profile_id)
+        self._mock_get_direct_reports(profile_id, direct_reports=0)
+
         response = self.client.call_action('get_categories', profile_id=profile_id)
         self.assertTrue(response.success)
         self.assertEqual(len(response.result.profile_categories), 1)
 
         category = response.result.profile_categories[0]
         self.assertEqual(category.title, 'Peers')
+        self.assertEqual(len(category.content), 3)
+        self.assertEqual(category.content_key, 'title')
+
+    def test_direct_reports_profile_category(self):
+        profile_id = fuzzy.FuzzyUUID().fuzz()
+        self._mock_get_peers(profile_id, peers=0)
+        self._mock_get_direct_reports(profile_id)
+
+        response = self.client.call_action('get_categories', profile_id=profile_id)
+        self.assertTrue(response.success)
+        self.assertEqual(len(response.result.profile_categories), 1)
+
+        category = response.result.profile_categories[0]
+        self.assertEqual(category.title, 'Direct Reports')
         self.assertEqual(len(category.content), 3)
         self.assertEqual(category.content_key, 'title')
