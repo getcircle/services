3a4a802bda80ed40179052169eb7db5dbeabbb01
Michael Hahn <mwhahn@gmail.com>
2015-01-14 23:35:05 -0800

    Ensure we're deleting any previously uploaded user image

--- media/actions.py
+++ media/actions.py
@@ -1,5 +1,11 @@
+import hashlib
+import urllib
+import uuid
+
+import arrow
 from django.conf import settings
 from protobufs.media_service_pb2 import MediaService
+from protobufs.profile_service_pb2 import ProfileService
 from service import (
     actions,
     validators,
@@ -10,7 +16,6 @@ from boto.s3.connection import (
     S3ResponseError,
 )
 from boto.s3.multipart import MultiPartUpload
-import uuid
 
 from . import utils
 
@@ -23,13 +28,16 @@ class StartImageUpload(actions.Action):
 
     def _validate_profile_id(self, profile_id):
         if not validators.is_uuid4(profile_id):
-            self.note_field_error('key', 'INVALID')
+            self.note_field_error('media_key', 'INVALID')
 
         # XXX FUCK!! really need to do errors
         if not self.is_error():
-            response = self.profile_client.call_action('get_profile', profile_id=self.request.key)
+            response = self.profile_client.call_action(
+                'get_profile',
+                profile_id=self.request.media_key,
+            )
             if not response.success:
-                self.note_field_error('key', 'DOES_NOT_EXIST')
+                self.note_field_error('media_key', 'DOES_NOT_EXIST')
 
     def _get_s3_connection(self):
         return S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
@@ -38,17 +46,21 @@ class StartImageUpload(actions.Action):
         connection = self._get_s3_connection()
         return connection.get_bucket(settings.AWS_S3_MEDIA_BUCKET)
 
+    def _get_image_identifier(self, key):
+        return hashlib.md5(arrow.utcnow().isoformat() + ':' + key).hexdigest()
+
     def _build_media_key(self):
         media_object = self.request.media_object
-        key = self.request.key
         if media_object == MediaService.PROFILE:
-            return 'profile-%s' % (uuid.UUID(key, version=4).hex)
+            return 'profiles/%s' % (
+                self._get_image_identifier(uuid.UUID(self.request.media_key, version=4).hex),
+            )
 
     def validate(self, *args, **kwargs):
         super(StartImageUpload, self).validate(*args, **kwargs)
         if not self.is_error():
             if self.request.media_object == MediaService.PROFILE:
-                self._validate_profile_id(self.request.key)
+                self._validate_profile_id(self.request.media_key)
 
     def run(self, *args, **kwargs):
         bucket = self._get_media_bucket()
@@ -61,6 +73,7 @@ class StartImageUpload(actions.Action):
             metadata={'content-type': 'image/png'},
         )
         self.response.upload_instructions.upload_id = response.id
+        self.response.upload_instructions.upload_key = media_key
 
         path = '/%s' % (media_key,)
         self.response.upload_instructions.upload_url = utils.get_presigned_url(
@@ -75,18 +88,53 @@ class StartImageUpload(actions.Action):
 
 class CompleteImageUpload(StartImageUpload):
 
-    def run(self, *args, **kwargs):
-        bucket = self._get_media_bucket()
-        media_key = self._build_media_key()
-        multipart_upload = MultiPartUpload(bucket)
+    def _complete_upload(self):
+        multipart_upload = MultiPartUpload(self.bucket)
         multipart_upload.id = self.request.upload_id
-        multipart_upload.key_name = media_key
+        multipart_upload.key_name = self.request.upload_key
 
         try:
             response = multipart_upload.complete_upload()
         except S3ResponseError as e:
             self.note_error('FAILED', ('FAILED', 'failed to complete upload: %s' % (e,)))
+            return
         except TypeError:
             self.note_field_error('upload_id', 'UNKNOWN')
+            return
         else:
             self.response.media_url = response.location
+
+    def _delete_previous_profile_image(self):
+        client = service.control.Client('profile', token=self.token)
+        response = client.call_action('get_profile', profile_id=self.request.media_key)
+        if not response.success:
+            print 'warning: failed to fetch profile'
+            return
+
+        old_image_url = response.result.profile.image_url
+        # update the profile with the new image and delete the previous image
+        # (we only do this if the update succeeds)
+        updated_profile = ProfileService.Containers.Profile()
+        updated_profile.CopyFrom(response.result.profile)
+        updated_profile.image_url = self.response.media_url
+        response = client.call_action('update_profile', profile=updated_profile)
+        # XXX should we rollback the image upload if this happens?
+        if not response.success:
+            print 'warning: failed to fetch profile'
+            return
+        else:
+            if old_image_url:
+                unquoted_key = urllib.unquote_plus(old_image_url)
+                key_parts = unquoted_key.rsplit('/')[-2:]
+                key_name = '/'.join(key_parts)
+                self.bucket.delete_key(key_name)
+
+    def _delete_previous_image(self):
+        if self.request.media_object == MediaService.PROFILE:
+            self._delete_previous_profile_image()
+
+    def run(self, *args, **kwargs):
+        self.bucket = self._get_media_bucket()
+        self._complete_upload()
+        if not self.is_error():
+            self._delete_previous_image()
--- media/tests.py
+++ media/tests.py
@@ -1,3 +1,6 @@
+import urllib
+
+from django.conf import settings
 from mock import (
     MagicMock,
     patch,
@@ -30,6 +33,10 @@ class TestMediaService(TestCase):
         profile.organization_id = fuzzy.FuzzyUUID().fuzz()
         profile.title = fuzzy.FuzzyText().fuzz()
         profile.full_name = fuzzy.FuzzyText().fuzz()
+        profile.image_url = 'https://%s.s3.amazonaws.com/%s' % (
+            settings.AWS_S3_MEDIA_BUCKET,
+            urllib.quote_plus('profiles/%s' % (fuzzy.FuzzyUUID().fuzz(),)),
+        )
 
     def _mock_get_profile(self, profile_id=None):
         service = 'profile'
@@ -39,14 +46,31 @@ class TestMediaService(TestCase):
         if profile_id is not None:
             mock_response.profile.id = profile_id
         mock.instance.register_mock_response(service, action, mock_response, profile_id=profile_id)
+        return mock_response.profile
+
+    def _mock_update_profile(self, profile=None):
+        service = 'profile'
+        action = 'update_profile'
+        mock_response = mock.get_mockable_response(service, action)
+        self._mock_profile(mock_response.profile)
+        if profile is not None:
+            mock_response.profile.CopyFrom(profile)
+        mock.instance.register_mock_response(
+            service,
+            action,
+            mock_response,
+            mock_regex_lookup=r'profile:update_profile:.*',
+            profile=mock_response.profile,
+        )
+        return mock_response.profile
 
     def test_start_image_upload_profile_invalid_key(self):
         response = self.client.call_action(
             'start_image_upload',
             media_object=MediaService.PROFILE,
-            key='invalid',
+            media_key='invalid',
         )
-        self._verify_field_error(response, 'key')
+        self._verify_field_error(response, 'media_key')
 
     def test_start_image_upload_profile_does_not_exist(self):
         # mock the get_profile call to return DOES_NOT_EXIST
@@ -63,9 +87,9 @@ class TestMediaService(TestCase):
         response = self.client.call_action(
             'start_image_upload',
             media_object=MediaService.PROFILE,
-            key=profile_id,
+            media_key=profile_id,
         )
-        self._verify_field_error(response, 'key', 'DOES_NOT_EXIST')
+        self._verify_field_error(response, 'media_key', 'DOES_NOT_EXIST')
 
     # TODO add tests for failing connections
     @patch('media.actions.S3Connection')
@@ -79,7 +103,7 @@ class TestMediaService(TestCase):
         response = self.client.call_action(
             'start_image_upload',
             media_object=MediaService.PROFILE,
-            key=profile_id,
+            media_key=profile_id,
         )
         self.assertTrue(response.success)
         self.assertTrue(response.result.upload_instructions.upload_id)
@@ -89,10 +113,10 @@ class TestMediaService(TestCase):
         response = self.client.call_action(
             'complete_image_upload',
             media_object=MediaService.PROFILE,
-            key='invalid',
+            media_key='invalid',
             upload_id='fake',
         )
-        self._verify_field_error(response, 'key')
+        self._verify_field_error(response, 'media_key')
 
     def test_complete_image_upload_profile_does_not_exist(self):
         # mock the get_profile call to return DOES_NOT_EXIST
@@ -109,33 +133,66 @@ class TestMediaService(TestCase):
         response = self.client.call_action(
             'complete_image_upload',
             media_object=MediaService.PROFILE,
-            key=profile_id,
+            media_key=profile_id,
             upload_id='fake',
         )
-        self._verify_field_error(response, 'key', 'DOES_NOT_EXIST')
+        self._verify_field_error(response, 'media_key', 'DOES_NOT_EXIST')
 
-    @patch('media.actions.MultiPartUpload')
-    @patch('media.actions.S3Connection')
-    def test_complete_image_upload_profile(self, mock_s3_connection, mock_multipart):
+    def _mock_complete_image_upload(self, mock_s3_connection, mock_multipart):
         # mock the s3 initiation call
         type(
             mock_s3_connection().get_bucket().initiate_multipart_upload()
         ).id = PropertyMock(return_value=fuzzy.FuzzyUUID().fuzz())
-
         # mock the multipart complete_upload
         mock_response = MagicMock()
         type(mock_response).location = PropertyMock(
-            return_value='https://otterbots-media.s3.amazonaws.com/media-id'
+            return_value='https://otterbots-media.s3.amazonaws.com/media-id/%s' % (
+                fuzzy.FuzzyUUID().fuzz(),
+            )
         )
         mock_multipart().complete_upload.return_value = mock_response
+
+    @patch('media.actions.MultiPartUpload')
+    @patch('media.actions.S3Connection')
+    def test_complete_image_upload_profile(self, mock_s3_connection, mock_multipart):
+        self._mock_complete_image_upload(mock_s3_connection, mock_multipart)
         profile_id = fuzzy.FuzzyUUID().fuzz()
-        self._mock_get_profile(profile_id)
+        profile = self._mock_get_profile(profile_id)
+        self._mock_update_profile(profile)
 
         response = self.client.call_action(
             'complete_image_upload',
             media_object=MediaService.PROFILE,
-            key=profile_id,
+            media_key=profile_id,
+            upload_key='profiles/%s' % (profile_id,),
             upload_id=fuzzy.FuzzyUUID().fuzz(),
         )
         self.assertTrue(response.success)
         self.assertTrue(response.result.media_url.startswith('https'))
+
+    @patch('media.actions.MultiPartUpload')
+    @patch('media.actions.S3Connection')
+    def test_complete_image_upload_profile_delete_previous_image(
+            self,
+            mock_s3_connection,
+            mock_multipart,
+        ):
+        self._mock_complete_image_upload(mock_s3_connection, mock_multipart)
+        profile_id = fuzzy.FuzzyUUID().fuzz()
+        profile = self._mock_get_profile(profile_id)
+        self._mock_update_profile(profile)
+
+        response = self.client.call_action(
+            'complete_image_upload',
+            media_object=MediaService.PROFILE,
+            media_key=profile_id,
+            upload_key='profiles/%s' % (profile_id,),
+            upload_id=fuzzy.FuzzyUUID().fuzz()
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(mock_s3_connection().get_bucket().delete_key.call_count, 1)
+
+        old_path = mock_s3_connection().get_bucket().delete_key.call_args[0][0]
+        # verify the path we deleted was urlencoded (path should be "profiles/<id>")
+        self.assertTrue(old_path.startswith('profiles'))
+        self.assertTrue(old_path.index('/') > 0)
