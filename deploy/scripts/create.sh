21c5fddb324e13f696a1c586737b53d50286d26c
Michael Hahn <mwhahn@gmail.com>
2015-01-18 10:09:09 -0800

    only support t1.micro right now

--- deploy/scripts/create.sh
+++ deploy/scripts/create.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-eb create $1 --cname "$1-circleapp" --instance_type t2.small --single --platform "Docker 1.3.3" --region "us-east-1" --database.password $2 --database.engine postgres --database.size 5 --tags "env=$1"
+eb create $1 --cname "$1-circleapp" --instance_type t1.micro --single --platform "Docker 1.3.3" --region "us-east-1" --database.password $2 --database.engine postgres --database.size 5 --tags "env=$1"
