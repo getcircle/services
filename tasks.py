9589adc873af80f7d1125de95b3211863a7d9932
Michael Hahn <mwhahn@gmail.com>
2015-01-24 15:24:35 -0800

    Add task for archiving the repo

--- tasks.py
+++ tasks.py
@@ -4,3 +4,7 @@ from invoke import task, run
 @task
 def test(app):
     run('./manage.py test -k --failfast %s' % (app,), pty=True)
+
+@task
+def archive(version):
+    run('git archive --format=zip HEAD > archives/services_%s.zip' % (version,))
