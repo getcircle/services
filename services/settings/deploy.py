f901d4880f878aff0f581c50ad1e1c4f50b523dd
Michael Hahn <mwhahn@gmail.com>
2015-01-28 15:10:18 -0800

    Make sure we're always defining DATABASES

--- services/settings/deploy.py
+++ services/settings/deploy.py
@@ -4,17 +4,14 @@ import urlparse
 # import default settings
 from . import *  # noqa
 
-if 'DATABASE_URL' in os.environ:
-    url = urlparse.urlparse(os.environ['DATABASE_URL'])
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        }
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
     }
-else:
-    print 'WARNING: "DATABASE_URL" not found'
+}
