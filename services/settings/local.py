5638f7531701c52ceb6ee70215111273e279b4c9
Michael Hahn <mwhahn@gmail.com>
2015-03-11 20:11:55 -0700

    Adjust local MAX_PAGE_SIZE

--- services/settings/local.py
+++ services/settings/local.py
@@ -1,6 +1,9 @@
 # import default settings
 from . import *  # noqa
 
+from service import settings
+settings.MAX_PAGE_SIZE = os.environ.get('MAX_PAGE_SIZE', 10000)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
