918813afbb7ad1cdcbd0777f55e55db79747b1c0
Michael Hahn <mwhahn@gmail.com>
2015-03-21 00:36:18 -0700

    Add rule to protect against duplicate values

---
+++ profiles/migrations/0002_auto_20150321_0735.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from common.db.migrations.operations import AddRule
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+       ('profiles', '0001_initial'),
+    ]
+
+    operations = [
+        AddRule(
+            'profiles_skill_on_duplicate_ignore',
+            'profiles_skill',
+            'INSERT',
+            'WHERE EXISTS (SELECT 1 FROM profiles_skill WHERE (name, organization_id) = (NEW.name, NEW.organization_id)) DO INSTEAD NOTHING',
+        )
+    ]
