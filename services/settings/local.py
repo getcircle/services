6596fd4a8cad6fae06a8205957b2c9d2851d1141
Michael Hahn <mwhahn@gmail.com>
2015-03-21 00:33:34 -0700

    Speed up unit tests with a faster hashing algorithm for user passwords

--- services/settings/local.py
+++ services/settings/local.py
@@ -4,6 +4,10 @@ from . import *  # noqa
 from service import settings
 settings.MAX_PAGE_SIZE = os.environ.get('MAX_PAGE_SIZE', 10000)
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
