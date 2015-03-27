5cef713141d4fb960c2bd023f7ceac5a490fd537
Michael Hahn <mwhahn@gmail.com>
2015-03-27 12:34:22 -0700

    Fix load_skills management command

--- onboarding/parsers/skills.py
+++ onboarding/parsers/skills.py
@@ -15,24 +15,25 @@ class Parser(OrganizationParser):
         with open(self.filename, 'r') as csvfile:
             reader = DictReader(csvfile)
             for row in reader:
-                skill = ProfileService.Containers.Skill()
+                skill = ProfileService.Containers.Tag()
+                skill.type = ProfileService.SKILL
                 skill.name = row['name']
                 self.debug_log('adding skill: %s' % (protobuf_to_dict(skill),))
                 skills.add(skill.SerializeToString())
 
-        deduped_skills = [ProfileService.Containers.Skill.FromString(t) for t in skills]
+        deduped_skills = [ProfileService.Containers.Tag.FromString(t) for t in skills]
         if kwargs.get('commit'):
             self.debug_log('saving %s skills' % (len(skills),))
             client = service.control.Client('profile', token=self.token)
             response = client.call_action(
-                'create_skills',
+                'create_tags',
                 organization_id=self.organization.id,
-                skills=deduped_skills,
+                tags=deduped_skills,
             )
             if not response.success:
                 raise ParseError('failed to create skills: %s' % (response.errors,))
 
-            created_count = len(response.result.skills)
+            created_count = len(response.result.tags)
             expected_count = len(skills)
             if created_count != expected_count:
                 raise ParseError(
