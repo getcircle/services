a935e36481b7579ca2c8c578b8a6084dd66fa03f
Michael Hahn <mwhahn@gmail.com>
2015-03-27 12:33:58 -0700

    Support resetting the db before creating the organization

--- onboarding/management/commands/create_organization.py
+++ onboarding/management/commands/create_organization.py
@@ -1,3 +1,7 @@
+from optparse import make_option
+import subprocess
+
+from django.db import connection
 import service.control
 
 from services.management.base import (
@@ -9,10 +13,26 @@ from services.management.base import (
 class Command(BaseCommand):
     args = '<organization name> <organization domain>'
     help = 'Creates an organization'
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--reset',
+            action='store_true',
+            dest='reset',
+            default=False,
+            help='Boolean for whether or not we should reset the schema before creating the org',
+        ),
+    )
 
     def handle(self, *args, **options):
         organization_name = args[0]
         organization_domain = args[1]
+
+        reset = options.get('reset')
+        if reset:
+            cursor = connection.cursor()
+            cursor.execute('DROP SCHEMA PUBLIC CASCADE')
+            cursor.execute('CREATE SCHEMA PUBLIC')
+            subprocess.call(['python', 'manage.py', 'migrate', '--noinput'])
 
         # XXX we shouldn't have an admin token, we should have some way of
         # generating one on the fly though
