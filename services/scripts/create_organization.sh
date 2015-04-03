05e77f04cffd2c7671d3fbbe548baa8ab1710fbe
Michael Hahn <mwhahn@gmail.com>
2015-04-03 05:51:17 -0700

    demo@circlehq.co should only be added to our circle company

--- services/scripts/create_organization.sh
+++ services/scripts/create_organization.sh
@@ -8,4 +8,4 @@ python manage.py create_organization $COMPANY_NAME $COMPANY_DOMAIN --reset &&
     python manage.py load_skills $COMPANY_DOMAIN --filename onboarding/fixtures/eventbrite_skills.csv --commit &&
     python manage.py make_new_hires $COMPANY_DOMAIN &&
     python manage.py add_skills $COMPANY_DOMAIN &&
-    python manage.py add_rhlabs_members $COMPANY_DOMAIN mwhahn@gmail.com ravirani@gmail.com demo@circlehq.co
+    python manage.py add_rhlabs_members $COMPANY_DOMAIN mwhahn@gmail.com ravirani@gmail.com
