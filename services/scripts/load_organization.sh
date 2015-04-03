fd235841ab810d3f4314b4a607e1802711b1547e
Michael Hahn <mwhahn@gmail.com>
2015-04-03 05:52:25 -0700

    Add load_organization script which won't reset the db

---
+++ services/scripts/load_organization.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COMPANY_NAME=$1
+COMPANY_DOMAIN=$2
+
+python manage.py create_organization $COMPANY_NAME $COMPANY_DOMAIN &&
+    python manage.py load_organization $COMPANY_DOMAIN --commit &&
+    python manage.py load_skills $COMPANY_DOMAIN --filename onboarding/fixtures/eventbrite_skills.csv --commit &&
+    python manage.py make_new_hires $COMPANY_DOMAIN &&
+    python manage.py add_skills $COMPANY_DOMAIN &&
+    python manage.py add_rhlabs_members $COMPANY_DOMAIN mwhahn@gmail.com ravirani@gmail.com
