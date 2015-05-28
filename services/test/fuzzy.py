49d1f462e4f9ed5b7dc0bcd8abfe2cc957d304ad
Michael Hahn <mwhahn@gmail.com>
2015-05-27 17:45:56 -0700

    Support returning a fuzzy raw UUID

--- services/test/fuzzy.py
+++ services/test/fuzzy.py
@@ -7,8 +7,15 @@ from factory.fuzzy import *  # NOQA
 
 class FuzzyUUID(BaseFuzzyAttribute):
 
+    def __init__(self, as_hex=True, *args, **kwargs):
+        self.as_hex = as_hex
+        super(FuzzyUUID, self).__init__(*args, **kwargs)
+
     def fuzz(self):
-        return uuid.uuid4().hex
+        value = uuid.uuid4()
+        if self.as_hex:
+            value = value.hex
+        return value
 
 
 class FuzzyTimestamp(BaseFuzzyAttribute):
