8af4adcc885488f0e6d4f7a7f5ed9f2bb3ce6dcc
Michael Hahn <mwhahn@gmail.com>
2015-06-08 01:11:21 -0700

    Properly add arguments to the command per django 1.8

--- services/management/commands/enable_google_groups.py
+++ services/management/commands/enable_google_groups.py
@@ -9,15 +9,28 @@ from organizations import models as organization_models
 
 
 class Command(BaseCommand):
-    help = 'One-off to add skills to profiles in an organization'
-    args = '<organization_domain> <admin_email>'
+    help = 'Enable the google groups integration for the given organization.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'organization_domain',
+            type=str,
+            help='Organization\'s domain',
+        )
+        parser.add_argument(
+            'admin_email',
+            type=str,
+            help='Google Admin email account for the organization',
+        )
 
     def handle(self, *args, **options):
-        organization = organization_models.Organization.objects.get(domain=args[0])
+        organization = organization_models.Organization.objects.get(
+            domain=options['organization_domain'],
+        )
         token = make_admin_token(organization_id=str(organization.id))
         client = service.control.Client('organization', token=token)
         response = client.call_action('enable_integration', integration={
             'integration_type': integration_pb2.GOOGLE_GROUPS,
-            'google_groups': {'admin_email': args[1]},
+            'google_groups': {'admin_email': options['admin_email']},
         })
         print protobuf_to_dict(response.result.integration)
