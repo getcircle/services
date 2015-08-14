10517ecd08defab44dbdedbda36efa79847bbbab
Michael Hahn <mwhahn@gmail.com>
2015-08-13 19:16:37 -0700

    Update add_rhlabs_members script

--- services/management/commands/add_rhlabs_members.py
+++ services/management/commands/add_rhlabs_members.py
@@ -4,6 +4,7 @@ import django.db
 from services.management.base import BaseCommand
 
 from organizations import models as organization_models
+from profiles import models as profile_models
 from services.test import fuzzy
 from users import models as user_models
 
@@ -26,24 +27,30 @@ class Command(BaseCommand):
         locked_users = options.get('lock')
         if locked_users:
             locked_users = user_models.User.objects.filter(primary_email__in=locked_users)
+            locked_profiles = profile_models.Profile.objects.filter(
+                user_id__in=[u.pk for u in locked_users],
+            )
 
-        queryset = organization_models.Team.objects.filter(organization_id=organization.id)
+        queryset = organization_models.ReportingStructure.objects.filter(
+            organization_id=organization.id,
+        )
         if locked_users:
             print 'locking users: %s' % (
                 ', '.join([locked_user.primary_email for locked_user in locked_users]),
             )
             queryset = queryset.exclude(
-                owner_id__in=[locked_user.id for locked_user in locked_users]
+                profile_id__in=[locked_profile.id for locked_profile in locked_profiles]
             )
 
         # NB: Don't include the CEO
-        teams = queryset.order_by('path')[1:len(emails) + 1]
+        managers = queryset.order_by('level')[1:len(emails) + 1]
         for index, email in enumerate(emails):
             print 'adding user: %s' % (email,)
+            profile = profile_models.Profile.objects.get(pk=managers[index].profile_id)
             try:
                 with django.db.transaction.atomic():
                     user_models.User.objects.filter(
-                        id=teams[index].owner_id,
+                        id=profile.user_id,
                     ).update(primary_email=email)
             except django.db.IntegrityError:
                 email_parts = email.split('@')
@@ -56,6 +63,6 @@ class Command(BaseCommand):
                 user_models.User.objects.filter(primary_email=email).update(
                     primary_email=new_email,
                 )
-                user_models.User.objects.filter(id=teams[index].owner_id).update(
-                    primary_email=email,
-                )
+                user_models.User.objects.filter(
+                    id=profile.user_id,
+                ).update(primary_email=email)
