df79cfd674deabd64f247bcf9693002bd4d7cc9f
Michael Hahn <mwhahn@gmail.com>
2015-08-18 14:35:17 -0700

    Wrap company name in quotes

--- services/scripts/create_organization.sh
+++ services/scripts/create_organization.sh
@@ -4,6 +4,6 @@ COMPANY_NAME=$1
 COMPANY_DOMAIN=$2
 COMPANY_IMAGE_URL=$3
 
-python manage.py create_organization $COMPANY_NAME $COMPANY_DOMAIN --reset --image_url "$COMPANY_IMAGE_URL" &&
+python manage.py create_organization "$COMPANY_NAME" $COMPANY_DOMAIN --reset --image_url "$COMPANY_IMAGE_URL" &&
     python manage.py load_organization $COMPANY_DOMAIN --commit &&
     python manage.py build_watson
