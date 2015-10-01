14ab6dcbb9883572883e38f4d90a61ba111ecb00
Michael Hahn <mwhahn@gmail.com>
2015-09-30 17:50:44 -0700

    Add an explicit endpoint for v1 api

--- services/urls.py
+++ services/urls.py
@@ -8,6 +8,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.ServicesView.as_view()),
+    url(r'^v1/$', views.ServicesView.as_view()),
     url(r'^p/', include('api.urls')),
     url(r'^user/', include('users.urls')),
 )
