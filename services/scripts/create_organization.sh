a558df3054bfb826cfd7dadf893de0485aae236d
Michael Hahn <mwhahn@gmail.com>
2015-10-04 16:40:54 -0700

    Have helper scripts take employee and locations file

--- services/scripts/create_organization.sh
+++ services/scripts/create_organization.sh
@@ -3,7 +3,9 @@
 COMPANY_NAME=$1
 COMPANY_DOMAIN=$2
 COMPANY_IMAGE_URL=$3
+COMPANY_EMPLOYEES_FILE=$4
+COMPANY_OFFICE_FILE=$5
 
 python manage.py create_organization "$COMPANY_NAME" $COMPANY_DOMAIN --reset --image_url "$COMPANY_IMAGE_URL" &&
-    python manage.py load_organization_v2 $COMPANY_DOMAIN --commit &&
+    python manage.py load_organization_v2 $COMPANY_DOMAIN --filename=$COMPANY_EMPLOYEES_FILE --locations-filename=$COMPANY_OFFICE_FILE --commit &&
     python manage.py build_watson
