cabc3d3c449655b07bc972474b54ee401872f694
Michael Hahn <mwhahn@gmail.com>
2015-10-29 02:03:54 -0700

    Remove search receivers, updates should come via API calls

--- search/apps.py
+++ search/apps.py
@@ -7,7 +7,6 @@ class AppConfig(BaseAppConfig):
     name = 'search'
 
     def ready(self):
-        from . import receivers  # NOQA - don't import signal receivers until the app is ready
         # create the elasticsearch connection
         connection_settings = settings.SEARCH_SERVICE_ELASTICSEARCH
         if connection_settings:
--- search/tests/models.py
+++
@@ -1,6 +0,0 @@
-from common.db import models
-
-
-class TestSearchModel(models.UUIDModel):
-    title = models.CharField(max_length=64)
-    content = models.CharField(max_length=255)
