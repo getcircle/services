de8d17d96ae9bb9e1e86537e4801dfa0fee97739
Michael Hahn <mwhahn@gmail.com>
2015-10-29 20:32:39 -0700

    Run bootstrap after a worker process starts

--- services/celery.py
+++ services/celery.py
@@ -2,7 +2,12 @@ from __future__ import absolute_import
 
 import os
 
-from celery import Celery
+from celery import (
+    Celery,
+    signals,
+)
+
+from .bootstrap import Bootstrap
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'services.settings')
 
@@ -12,3 +17,10 @@ app = Celery('celery')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+def on_worker_process_init(*args, **kwargs):
+    Bootstrap.bootstrap()
+
+
+signals.worker_process_init.connect(on_worker_process_init, dispatch_uid='bootstrap')
