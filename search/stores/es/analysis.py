5ecb24776885b046d1a2299c15d4adced1258b14
Michael Hahn <mwhahn@gmail.com>
2015-10-29 20:31:16 -0700

    Setup the edge_ngram filter properly

--- search/stores/es/analysis.py
+++ search/stores/es/analysis.py
@@ -1,8 +1,18 @@
-from elasticsearch_dsl import char_filter
+from elasticsearch_dsl import (
+    char_filter,
+    token_filter,
+)
 
 parens_strip = char_filter(
     'parens_strip',
     type='pattern_replace',
     pattern='[\(\)]',
     replacement='',
+)
+
+edge_ngram_max_gram_20 = token_filter(
+    'edge_ngram_max_gram_20',
+    type='edgeNGram',
+    min_gram=1,
+    max_gram=20,
 )
--- search/stores/es/types/profile/analysis.py
+++ search/stores/es/types/profile/analysis.py
@@ -1,16 +1,20 @@
 from elasticsearch_dsl import analyzer
-from ...analysis import parens_strip
+from ...analysis import (
+    edge_ngram_max_gram_20,
+    parens_strip,
+)
+
 
 full_name_analyzer_v1 = analyzer(
     'profile_full_name_analyzer_v1',
     tokenizer='standard',
-    filter=['standard', 'lowercase', 'edgeNGram'],
+    filter=['standard', 'lowercase', edge_ngram_max_gram_20],
     char_filter=[parens_strip],
 )
 
 display_title_analyzer_v1 = analyzer(
     'profile_display_title_analyzer_v1',
     tokenizer='standard',
-    filter=['standard', 'lowercase', 'edgeNGram'],
+    filter=['standard', 'lowercase', edge_ngram_max_gram_20],
     char_filter=[parens_strip],
 )
