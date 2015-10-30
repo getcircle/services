3ddf78e91ee0fb15bc2f4f0717ad7a0eeff67c19
Michael Hahn <mwhahn@gmail.com>
2015-10-30 14:23:33 -0500

    Wait for the primary nodes to get assigned
    Before we complete the index migration, we should wait until the primary
    node has been allocated. This means dependent migrations should pass.

--- search/stores/es/indices/search_v1/actions.py
+++ search/stores/es/indices/search_v1/actions.py
@@ -1,3 +1,5 @@
+import time
+
 from . import INDEX
 
 
@@ -5,3 +7,9 @@ def create_index(*args, **kwargs):
     INDEX.aliases(search={})
     INDEX.delete(ignore=404)
     INDEX.create()
+    waiting = True
+    while waiting:
+        health = INDEX.connection.cluster.health()
+        waiting = health['status'] != 'red'
+        print 'waiting for cluster to get out of "red" status: %s' % (health,)
+        time.sleep(1)
