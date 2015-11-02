692e660447c7ce4a827c7c17549cf796583af81f
Michael Hahn <mwhahn@gmail.com>
2015-11-02 13:27:39 -0800

    Use an edge_ngram up to 20 characters

--- search/stores/es/types/team/analysis.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch_dsl import analyzer
-from ...analysis import parens_strip
-
-name_analyzer_v1 = analyzer(
-    'team_name_analyzer_v1',
-    tokenizer='standard',
-    filter=['standard', 'lowercase', 'edgeNGram'],
-    char_filter=[parens_strip],
-)
--- search/stores/es/types/team/document.py
+++ search/stores/es/types/team/document.py
@@ -1,17 +1,29 @@
 from elasticsearch_dsl import (
+    analyzer,
     MetaField,
     String,
 )
 from protobufs.services.organization import containers_pb2 as organization_containers
 
+from ...analysis import (
+    edge_ngram_max_gram_20,
+    parens_strip,
+)
 from ...indices import search_v1
 from ..base import BaseDocType
-from . import analysis
+
+
+name_analyzer_v1 = analyzer(
+    'team_name_analyzer_v1',
+    tokenizer='standard',
+    filter=['standard', 'lowercase', edge_ngram_max_gram_20],
+    char_filter=[parens_strip],
+)
 
 
 @search_v1.INDEX.doc_type
 class TeamV1(BaseDocType):
-    name = String(analyzer=analysis.name_analyzer_v1)
+    name = String(analyzer=name_analyzer_v1)
     description = String(analyzer='english')
     organization_id = String(index='not_analyzed')
 
