a584eb943a8e75c007d160ed7c9d0eac2ebe396e
Michael Hahn <mwhahn@gmail.com>
2015-11-04 00:15:17 -0800

    Allow easier importing of es types

---
+++ search/stores/es/types/__init__.py
@@ -0,0 +1,4 @@
+from .location.document import LocationV1  # NOQA
+from .post.document import PostV1  # NOQA
+from .profile.document import ProfileV1  # NOQA
+from .team.document import TeamV1  # NOQA
