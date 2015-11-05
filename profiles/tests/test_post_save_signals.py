2d450f96340c1e0605b5b1b44b8d687900a1783c
Michael Hahn <mwhahn@gmail.com>
2015-11-04 19:14:27 -0800

    Remove test around ContactMethod post_save hook
    We removed this receiver

--- profiles/tests/test_post_save_signals.py
+++ profiles/tests/test_post_save_signals.py
@@ -4,10 +4,7 @@ from services.test import (
     TestCase,
 )
 
-from .. import (
-    factories,
-    models,
-)
+from .. import factories
 
 
 class TestPostSaveSignals(TestCase):
@@ -20,21 +17,5 @@ class TestPostSaveSignals(TestCase):
         call_args = patched.call_args_list[0][0]
         self.assertEqual(call_args, (instance.id, instance.organization_id))
         instance.first_name = fuzzy.FuzzyText().fuzz()
-        instance.save()
-        self.assertEqual(patched.call_count, 2)
-
-    @patch('profiles.receivers._update_profile_entity')
-    def test_post_save_signal_contact_method(self, patched):
-        profile = factories.ProfileFactory.create()
-        instance = models.ContactMethod(
-            profile=profile,
-            value='test',
-            organization_id=profile.organization_id,
-        )
-        instance.save()
-        self.assertEqual(patched.call_count, 1)
-        call_args = patched.call_args_list[0][0]
-        self.assertEqual(call_args, (instance.profile_id, instance.organization_id))
-        instance.value = fuzzy.FuzzyText().fuzz()
         instance.save()
         self.assertEqual(patched.call_count, 2)
