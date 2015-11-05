c1cdef005726bc47ab21475b95604b89820f59cc
Michael Hahn <mwhahn@gmail.com>
2015-11-05 11:03:33 -0800

    Handle AWS ES Service not supporting open and close index

--- search/stores/es/indices/actions.py
+++ search/stores/es/indices/actions.py
@@ -1,11 +1,16 @@
 import contextlib
 
+from django.conf import settings
 from elasticsearch_dsl import connections
 
 
 @contextlib.contextmanager
 def closed_index(index_name):
-    connection = connections.connections.get_connection()
-    connection.indices.close(index_name)
+    # AWS ES Service doesn't support close/open index at the moment
+    is_aws_es = settings.SEARCH_SERVICE_ELASTICSEARCH_URL.endswith('es.amazonaws.com')
+    if not is_aws_es:
+        connection = connections.connections.get_connection()
+        connection.indices.close(index_name)
     yield
-    connection.indices.open(index_name)
+    if not is_aws_es:
+        connection.indices.open(index_name)
