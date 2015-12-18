50d158365cc71d66cbf36351e75d84b4b230eb8e
Michael Hahn <mwhahn@gmail.com>
2015-12-18 12:20:35 -0800

    Add a shingle filter for display_title

--- search/stores/es/types/profile/document.py
+++ search/stores/es/types/profile/document.py
@@ -10,6 +10,7 @@ from ...analysis import (
     edge_ngram_tokenizer_v1,
     parens_char_filter_v1,
     raw_search,
+    shingle_filter,
 )
 
 full_name_analyzer_v1 = analyzer(
@@ -22,6 +23,13 @@ display_title_analyzer_v1 = analyzer(
     'profile_display_title_analyzer_v1',
     tokenizer=edge_ngram_tokenizer_v1,
     filter=['standard', 'lowercase'],
+    char_filter=[parens_char_filter_v1],
+)
+
+display_title_shingle_analyzer_v1 = analyzer(
+    'display_title_shingle_analyzer_v1',
+    tokenizer='standard',
+    filter=['standard', 'lowercase', shingle_filter],
     char_filter=[parens_char_filter_v1],
 )
 
@@ -43,6 +51,12 @@ class ProfileV1(BaseDocType):
         analyzer=display_title_analyzer_v1,
         search_analyzer='default_search',
         term_vector='with_positions_offsets',
+        fields={
+            'shingle': String(
+                analyzer=display_title_shingle_analyzer_v1,
+                term_vector='with_positions_offsets',
+            ),
+        },
     )
 
     class Meta:
