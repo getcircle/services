71c28dd299cfa6b336695aa9e29268a2a71c3a36
Michael Hahn <mwhahn@gmail.com>
2015-12-21 17:48:40 -0800

    Pass in the django request to get pulled in by sentry

--- hooks/email/views.py
+++ hooks/email/views.py
@@ -23,29 +23,29 @@ class ProcessEmailView(APIView):
 
         source = request.data.get('source')
         if not source:
-            logger.error('source not found', extra={'data': request.data})
+            logger.error('source not found', extra={'request': request._request})
             raise exceptions.ParseError('source is required')
 
         try:
             ses_source = request.data['recipients']
         except KeyError:
-            logger.error('recipients not provided', extra={'data': request.data})
+            logger.error('recipients not provided', extra={'request': request._request})
             raise exceptions.ParseError('recipients is required')
 
         if not isinstance(ses_source, basestring):
-            logger.error('recipients not a string', extra={'data': request.data})
+            logger.error('recipients not a string', extra={'request': request._request})
             raise exceptions.ParseError('recipients must be a string')
 
         domain = actions.extract_domain(source, ses_source)
         request.source = actions.get_details_for_source(domain, source)
         if not request.source:
-            logger.error('unknown source', extra={'data': request.data})
+            logger.error('unknown source', extra={'request': request._request})
             raise exceptions.NotFound()
 
     def post(self, request, *args, **kwargs):
         message_id = request.data.get('message_id')
         if not message_id:
-            logger.error('message_id is required', extra={'data': request.data})
+            logger.error('message_id is required', extra={'request': request._request})
             raise exceptions.ParseError('message_id is required')
 
         # XXX draft should be something the view accepts as a parameter
