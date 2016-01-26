40500c8e2ede80402c833d2542ba4d42cc718db9
Michael Hahn <mwhahn@gmail.com>
2016-01-25 22:51:14 -0800

    Fix user migration

--- users/migrations/0028_auto_20160115_0501.py
+++ users/migrations/0028_auto_20160115_0501.py
@@ -10,41 +10,61 @@ def populate_organization_id(apps, schema_editor):
     profile_dict = dict((p.user_id, p) for p in Profile.objects.all())
     user_dict = {}
     for user in User.objects.all():
-        profile = profile_dict[user.id]
-        user.organization_id = profile.organization_id
-        user.save()
-        user_dict[user.id] = user
+        profile = profile_dict.get(user.id)
+        if profile:
+            user.organization_id = profile.organization_id
+            user.save()
+            user_dict[user.id] = user
+        else:
+            print 'deleting user: %s' % (user.__dict__,)
+            user.delete()
 
     Device = apps.get_model('users', 'Device')
     for device in Device.objects.all():
-        user = user_dict[device.user_id]
-        device.organization_id = user.organization_id
-        device.save()
+        user = user_dict.get(device.user_id)
+        if user:
+            device.organization_id = user.organization_id
+            device.save()
+        else:
+            print 'deleting device: %s' % (device.__dict__,)
+            device.delete()
 
     Identity = apps.get_model('users', 'Identity')
     for identity in Identity.objects.all():
-        user = user_dict[identity.user_id]
-        identity.organization_id = user.organization_id
-        identity.save()
+        user = user_dict.get(identity.user_id)
+        if user:
+            identity.organization_id = user.organization_id
+            identity.save()
+        else:
+            print 'deleting identity: %s' % (identity.__dict__,)
+            identity.delete()
 
     Token = apps.get_model('users', 'Token')
     for token in Token.objects.all():
-        user = user_dict[token.user_id]
-        token.organization_id = user.organization_id
-        token.save()
+        user = user_dict.get(token.user_id)
+        if user:
+            token.organization_id = user.organization_id
+            token.save()
+        else:
+            print 'deleting token: %s' % (token.__dict__,)
+            token.delete()
 
     TOTPToken = apps.get_model('users', 'TOTPToken')
     for token in TOTPToken.objects.all():
-        user = user_dict[token.user_id]
-        token.organization_id = user.organization_id
-        token.save()
+        user = user_dict.get(token.user_id)
+        if user:
+            token.organization_id = user.organization_id
+            token.save()
+        else:
+            print 'deleting token: %s' % (token.__dict__,)
+            token.delete()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('users', '0027_auto_20160115_0501'),
-        ('profiles', '0001_initial'),
+        ('profiles', '0026_auto_20160120_1748'),
     ]
 
     operations = [
