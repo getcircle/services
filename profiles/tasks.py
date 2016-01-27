f4ca31fd038926babcbd8a737f9aa94a705d2aee
Michael Hahn <mwhahn@gmail.com>
2016-01-27 10:41:32 -0800

    Use a chord so we're not waiting on results in the thread

--- profiles/tasks.py
+++ profiles/tasks.py
@@ -1,7 +1,7 @@
 import logging
 import time
 
-from celery import group
+from celery import chord
 
 from services.celery import app
 
@@ -13,20 +13,24 @@ logger = logging.getLogger(__name__)
 TIME_LIMIT = 60 * 5
 
 
-@app.task(time_limit=TIME_LIMIT, soft_time_limit=TIME_LIMIT)
+@app.task
 def sync_all():
+    """Trigger syncing for all registered organizations"""
     start = time.time()
     logger.info('starting profile sync for registered organizations')
     organization_ids = SyncSettings.objects.all().values_list('organization_id', flat=True)
-    result = group(
-        sync_organization.s(str(organization_id)) for organization_id in organization_ids
-    )()
-    result.get()
-    end = time.time()
-    logger.info('completed syncing %d organizations (%d secs)', len(organization_ids), end - start)
+    chord(sync_organization.s(str(organization_id)) for organization_id in organization_ids)(
+        report_sync_stats.s(start, len(organization_ids)),
+    )
 
 
 @app.task(time_limit=TIME_LIMIT, soft_time_limit=TIME_LIMIT, ignore_result=False)
 def sync_organization(organization_id):
     settings = SyncSettings.objects.get(pk=organization_id)
     okta.sync(settings)
+
+
+@app.task
+def report_sync_stats(start, number):
+    end = time.time()
+    logger.info('completed syncing %d organizations (%d secs)', number, end - start)
