dd4f9c76437bc3e5604f1ea5d851d121d80864d5
Michael Hahn <mwhahn@gmail.com>
2016-01-27 01:28:41 -0800

    Create celery tasks to run the sync process
    This lets us set them up as PeriodicTasks

---
+++ profiles/tasks.py
@@ -0,0 +1,28 @@
+import logging
+import time
+
+from celery import group
+
+from services.celery import app
+
+from .models import SyncSettings
+from .sync.providers import okta
+
+logger = logging.getLogger(__name__)
+
+
+@app.task
+def sync_all():
+    start = time.time()
+    logger.info('starting profile sync for registered organizations')
+    organization_ids = SyncSettings.objects.all().values_list('organization_id', flat=True)
+    result = group(sync_organization.s(organization_id) for organization_id in organization_ids)
+    result.get()
+    end = time.time()
+    logger.info('completed syncing %d organizations (%d secs)', len(organization_ids), end - start)
+
+
+@app.task
+def sync_organization(organization_id):
+    settings = SyncSettings.objects.get(pk=organization_id)
+    okta.sync(settings)
