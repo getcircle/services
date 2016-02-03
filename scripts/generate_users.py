9f5d281a659ba8a94cd4a0e85d64df7a6f45bb9d
Michael Hahn <mwhahn@gmail.com>
2016-02-03 13:10:47 -0800

    Add script for generating fake users in a domain

---
+++ scripts/generate_users.py
@@ -0,0 +1,54 @@
+"""Script for creating fake users in dev within an organization.
+
+Run with:
+
+    $ python manage.py runscript generate_users --script-args team 10
+
+"""
+import logging
+
+from django.conf import settings
+import requests
+
+from services.bootstrap import Bootstrap
+
+from organizations.models import Organization
+from profiles.models import Profile
+from users.models import User
+
+logger = logging.getLogger(__name__)
+
+
+def generate_fake_user(organization_id, fake_data):
+    user = User.objects.create(
+        primary_email=fake_data['email'],
+        organization_id=organization_id,
+    )
+    Profile.objects.create(
+        user_id=user.id,
+        organization_id=organization_id,
+        email=fake_data['email'],
+        first_name=fake_data['name']['first'].capitalize(),
+        last_name=fake_data['name']['last'].capitalize(),
+        image_url=fake_data['picture']['medium'],
+        authentication_identifier=fake_data['email'],
+    )
+
+
+def run(domain, number_of_users):
+    if not settings.DEBUG:
+        logger.error('can\'t generate fake users in production')
+        return
+
+    Bootstrap.bootstrap()
+    endpoint = 'https://randomuser.me/api/?results=%s&nat=us' % (number_of_users,)
+    response = requests.get(endpoint)
+    if not response.ok:
+        logger.error('invalid response: %s - %s', response.status_code, response.reason)
+        return
+
+    organization = Organization.objects.get(domain=domain)
+    for fake_data in response.json()['results']:
+        generate_fake_user(organization.id, fake_data['user'])
+
+    logger.info('generated %s users in %s', number_of_users, domain)
