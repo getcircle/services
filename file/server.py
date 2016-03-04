fafc8183c9d6074eb293ad67270ce38a1cf289c0
Felix Mo <felix@felixmo.com>
2016-03-04 16:12:46 -0500

    Add TODO to remove unused action

--- file/server.py
+++ file/server.py
@@ -12,5 +12,6 @@ class Server(service.control.Server):
         'get_files': actions.GetFiles,
         'start_upload': actions.StartUpload,
         'upload': actions.Upload,
+        # TODO remove unused
         'get_file': actions.GetFile,
     }
