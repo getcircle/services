bcb27dda05f8e75064b4003e42d2de91b514ba1c
Felix Mo <felix@felixmo.com>
2016-03-09 00:04:07 -0500

    Defer constraint check

--- post/migrations/0012_add_position_field.py
+++ post/migrations/0012_add_position_field.py
@@ -4,6 +4,18 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+class CollectionDeferredUniqueTogether(migrations.AlterUniqueTogether):
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        new_model = to_state.apps.get_model(app_label, self.name)
+        if self.allow_migrate_model(schema_editor.connection.alias, new_model):
+            statement = schema_editor._create_unique_sql(
+                new_model,
+                ('organization_id', 'owner_id', 'owner_type', 'position'),
+            )
+            statement += ' INITIALLY DEFERRED'
+            schema_editor.execute(statement)
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -18,6 +30,10 @@ class Migration(migrations.Migration):
         ),
         migrations.AlterUniqueTogether(
             name='collection',
-            unique_together=set([('organization_id', 'owner_id', 'owner_type', 'is_default'), ('organization_id', 'owner_id', 'owner_type', 'position')]),
+            unique_together=set([('organization_id', 'owner_id', 'owner_type', 'is_default')]),
+        ),
+        CollectionDeferredUniqueTogether(
+            name='collection',
+            unique_together=set([('organization_id', 'owner_id', 'owner_type', 'position')]),
         ),
     ]
